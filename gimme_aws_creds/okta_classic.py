62c189c3669f2067322f23d9392192c2550ebdf9
Pepe Barbe <pepe@bright.ai>
2022-10-28 13:41:05 -0500

    Specify TOPT instance for preferred_mfa_type
    Apply change suggested here:
    
    https://github.com/Nike-Inc/gimme-aws-creds/issues/87#issuecomment-930032729

--- gimme_aws_creds/okta_classic.py
+++ gimme_aws_creds/okta_classic.py
@@ -831,7 +831,7 @@ class OktaClassicClient(object):
             passcode['factorType'] = "passcode"
             factors.append(passcode)
         if self._preferred_mfa_type is not None:
-            preferred_factors = list(filter(lambda item: item['factorType'] == self._preferred_mfa_type, factors))
+            preferred_factors = list(filter(lambda item: self._build_factor_name(item) == self._preferred_mfa_type, factors))
             # If the preferred factor isn't in the list of available factors, we'll let the user know before
             # prompting to select another.
             if not preferred_factors:
